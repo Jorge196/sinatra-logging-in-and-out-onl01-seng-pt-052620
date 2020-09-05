3370e4e08aa0c5abd219701318ca289563f9fb65
Jorge Ramirez <joramirez96@gmail.com>
2020-09-05 18:53:44 +0000

    Automatically backed up by Learn

--- db/migrate/20200905185131_users.rb
+++ db/migrate/20200905185131_users.rb
@@ -1,6 +1,9 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end 
   end
 end
